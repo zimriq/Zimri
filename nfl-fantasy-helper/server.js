a3b4dffad51862fbd53f3f97f0e57d14124cd9f1
Zimri Quinonez <zimryquinonez@gmail.com>
2025-11-04 00:08:26 -0500

    Starting new proj, creating server

---
+++ nfl-fantasy-helper/server.js
@@ -0,0 +1,9 @@
+//import libraries
+const express = require('express'); 
+const axios = require('axios'); 
+const cors = require('cors'); 
+
+//create server
+const app = express(); 
+const PORT = 3000; 
+
