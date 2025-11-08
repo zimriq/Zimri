eb3ec0ff8be91980caa038b730e5cccf5f5cff3e
Zimri Quinonez <zimryquinonez@gmail.com>
2025-11-08 00:54:44 -0500

    Steps 1-3 finished under Start/Sit route

--- nfl-fantasy-helper/server.js
+++ nfl-fantasy-helper/server.js
@@ -50,6 +50,58 @@ app.get('/api/player/:name', async (req, res) => {
 });
 
 
+//Start/Sit Recommendation
+app.get('/api/compare', async (req, res) => {
+    try {
+    //getting players from query params
+    const playerNames = req.query.players; 
+
+    //handling player names properly
+    if(!playerNames){
+        return res.status(400).json ({ error: 'Please provide player names'});
+    }
+
+    const playerArray = playerNames.split(',').map(name => name.trim()); 
+
+    if(playerArray.length < 2){
+        return res.status(400).json({ error: 'Please provide at least 2 players to compare'});
+    }
+
+    //fetching players: 
+    const response = await axios.get('https://api.sleeper.app/v1/players/nfl');
+    const allPlayers = response.data;
+
+
+    //finding each player 
+    let foundPlayers = []; //empty array
+
+    for(let i = 0; i < playerArray.length; i++){
+        const currentPlayerName = playerArray[i];
+
+        const matchedPlayer = Object.values(allPlayers).find(p =>
+            p.full_name && p.full_name.toLowerCase().includes(currentPlayerName.toLowerCase())
+        );
+
+        if(matchedPlayer){  //adding to array 
+            foundPlayers.push(matchedPlayer); 
+        }
+    }
+    
+    //checking if all players found
+    if(foundPlayers.length !== playerArray.length){
+        return res.status(400).json ({ error: 'One or more players not found'});
+    }
+
+    //TODO: step 4 - calculate scores 
+    //TODO: step 5 - Determine recommendation
+    //TODO: step 6 - Send response 
+    
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to compare players' }); 
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
